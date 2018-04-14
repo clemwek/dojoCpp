0e1b5e774d102bad29702bb5a9d87bb617d35b9b
Dhwty <clemwek@gmail.com>
2018-04-14 06:34:55 +0300

    Project setup

---
+++ TestDojo/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  TestDojo
+//
+//  Created by Clement Wekesa on 14/04/2018.
+//  Copyright © 2018 Clement Wekesa. All rights reserved.
+//
+
+#define CATCH_CONFIG_MAIN
+
+#include "catch.hpp"
+
+TEST_CASE("first test") {
+    
+}
