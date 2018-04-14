a29709610fcda4e03113f6816fad78c1ba79c58b
Clement Wekesa <dhwty@clements-macbook-pro.local>
2018-04-14 06:06:20 +0300

    Initial Commit

---
+++ Dojo/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Dojo
+//
+//  Created by Clement Wekesa on 14/04/2018.
+//  Copyright © 2018 Clement Wekesa. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
