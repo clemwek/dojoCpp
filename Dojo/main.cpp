b18a027faf0e8f49410d8d7fdb9f39204cf7cdd2
_dhwty_ <clemwek@users.noreply.github.com>
2018-04-14 06:45:50 +0300

    Create README.md

--- Dojo/main.cpp
+++ Dojo/main.cpp
@@ -8,8 +8,28 @@
 
 #include <iostream>
 
+using namespace std;
+
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    std::string command;
+    
+    std::cout << "Please enter a command." <<std::endl;
+
+    while (command != "q") {
+        std::getline(cin, command);
+
+        std::cout << command;
+    }
+    
+//    string str1("the brown fox jumped over the lazy dog");
+//    istringstream iss(str1);
+//
+//    while (iss) {
+//        string word;
+//        iss >> word;
+//        cout << word << endl;
+//    }
     return 0;
 }
---
+++ README.md
@@ -0,0 +1,10 @@
+# dojoCpp
+
+This is a command line program in C++
+
+
+## Description
+
+When a new Fellow joins Andela they are assigned an office space and an optional living space if they choose to opt in. When a new Staff joins they are assigned an office space only. In this exercise I am digitize and randomize a room allocation system for one of Andela Kenya’s facilities called The Dojo.
+
+### Installing
